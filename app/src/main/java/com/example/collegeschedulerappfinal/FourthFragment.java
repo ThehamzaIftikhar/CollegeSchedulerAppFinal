5b2d811818d3e6d020f42df262ecd95c4b7dfe36
ayubxhunter <a_hunter65@gatech.edu>
2024-02-02 15:39:20 -0500

    Finished Home Page2

---
+++ app/src/main/java/com/example/collegeschedulerappfinal/FourthFragment.java
@@ -0,0 +1,42 @@
+package com.example.collegeschedulerappfinal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.collegeschedulerappfinal.databinding.FragmentSecondBinding;
+
+public class FourthFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FourthFragment.this)
+                        .navigate(R.id.action_FourthFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
